eb92c9b21aaa00f912fe5c2e5188abc47fda5211
Thao Le <lethao@deakin.edu.au>
2020-05-06 16:37:54 +1000

    correct incorrect GloVe emb file

--- data/glove/txt2pickle.py
+++ data/glove/txt2pickle.py
@@ -2,9 +2,9 @@ import pandas as pd
 import pickle
 import csv
 
-df = pd.read_csv('glove.6B.300d.txt', sep= " ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
+df = pd.read_csv('glove.840B.300d.txt', sep =" ", quoting=3, header=None, index_col=0)
 print("glove file loaded!")
 glove = {key: val.values for key, val in df.T.items()}
 
-with open('glove.6B.300d.pkl', 'wb') as fp:
+with open('glove.840.300d.pkl', 'wb') as fp:
     pickle.dump(glove, fp)
